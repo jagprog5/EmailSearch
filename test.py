9c7b234a155a486be99c0c7e7f98ba83aef34b7d
john <johngiorshev@gmail.com>
2022-02-16 10:10:05 -0500

    Edit patterns

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     assert(extract(patterns, " some text github.com/username-HERE123") == ["github.com/username-HERE123"])
     assert(extract(patterns, " some text gitlab.com/test0, and gitlab.com/test1") == ["gitlab.com/test0", "gitlab.com/test1"])
     assert(extract(patterns, " my-name-is.github.io a") == ["my-name-is.github.io"])
-    assert(extract(patterns, " GitHub: /tester-here123 ") == ["GitHub: /tester-here123"])
-    assert(extract(patterns, " gitlab: tester-here123 ") == ["gitlab: tester-here123"])
+    assert(extract(patterns, "here's my GitHub: /tester-here123 ") == ["GitHub: /tester-here123"])
+    assert(extract(patterns, " gitlab:tester-here123 ") == ["gitlab:tester-here123"])
     print("good!")
